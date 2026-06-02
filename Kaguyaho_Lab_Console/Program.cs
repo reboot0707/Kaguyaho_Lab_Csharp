966469a20b42afba8005d3f182bcf568d119fdf8
reboot0707 <bootcamp@contact.clam.tw>
2026-06-02 10:08:06 +0800

    加入專案檔案。

---
+++ Kaguyaho_Lab_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaguyaho_Lab_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
