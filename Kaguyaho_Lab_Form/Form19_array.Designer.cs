bd3a3a9bec1a672162b19dbdeb1e9c8a434e96e0
reboot0707 <bootcamp@contact.clam.tw>
2026-06-09 12:55:09 +0800

    feat: 測試陣列裡面放類別

--- Kaguyaho_Lab_Form/Form19_array.Designer.cs
+++ Kaguyaho_Lab_Form/Form19_array.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_trigArray = new System.Windows.Forms.Button();
+            this.button_testArray2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_trigArray
+            // 
+            this.button_trigArray.Location = new System.Drawing.Point(285, 120);
+            this.button_trigArray.Name = "button_trigArray";
+            this.button_trigArray.Size = new System.Drawing.Size(293, 149);
+            this.button_trigArray.TabIndex = 0;
+            this.button_trigArray.Text = "test Array";
+            this.button_trigArray.UseVisualStyleBackColor = true;
+            this.button_trigArray.Click += new System.EventHandler(this.button_trigArray_Click);
+            // 
+            // button_testArray2
+            // 
+            this.button_testArray2.Location = new System.Drawing.Point(126, 289);
+            this.button_testArray2.Name = "button_testArray2";
+            this.button_testArray2.Size = new System.Drawing.Size(293, 149);
+            this.button_testArray2.TabIndex = 1;
+            this.button_testArray2.Text = "test Array 2";
+            this.button_testArray2.UseVisualStyleBackColor = true;
+            this.button_testArray2.Click += new System.EventHandler(this.button_testArray2_Click);
+            // 
+            // Form19_array
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_testArray2);
+            this.Controls.Add(this.button_trigArray);
+            this.Name = "Form19_array";
             this.Text = "Form19_array";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_trigArray;
+        private System.Windows.Forms.Button button_testArray2;
     }
 }
--- Kaguyaho_Lab_Form/Form19_array.cs
+++ Kaguyaho_Lab_Form/Form19_array.cs
@@ -16,5 +16,68 @@ namespace Kaguyaho_Lab_Form
         {
             InitializeComponent();
         }
+
+        private void button_trigArray_Click(object sender, EventArgs e)
+        {
+            int[] arr = new int[6];
+            arr[0] = 1;
+            arr[1] = 1;
+            arr[2] = 4;
+            arr[3] = 5;
+            arr[4] = 1;
+            arr[5] = 4;
+
+            int index = 0;
+            string msgResult = string.Empty;
+            foreach (int i in arr)
+            {
+                msgResult += $"arr[{index}] => {i}\n";
+                index++;
+            }
+            MessageBox.Show(msgResult);
+
+            string msgResult2 = string.Empty;
+            for (int i=0; i<arr.Length; i++)
+            {
+                msgResult2 += $"arr[{i}] => {arr[i]}\n";
+            }
+            MessageBox.Show(msgResult2);
+
+            string msgResult3 = string.Empty;
+            
+            int[,] arr2d = new int[2,3] { { 1,1,4 },{ 5,1,4 } };
+            for (int i = 0; i < arr2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2d.GetLength(1); j++)
+                {
+                    msgResult += $"{arr2d[i,j]}";
+                }
+            }
+            MessageBox.Show("!!\n" + msgResult2);
+        }
+
+        private void button_testArray2_Click(object sender, EventArgs e)
+        {
+            Member[] members = new Member[2]
+            { 
+                new Member() { id = 0, Name = "John", Phone = "0987654321" },
+                new Member() { id = 1, Name = "Cena", Phone = "0987654567" },
+            };
+            //members[0].id = 0;
+            //members[0].Name = "John";
+            //members[0].Phone = "0912345678";
+            //members[1].id = 1;
+            //members[1].Name = "Wick";
+            //members[1].Phone = "026666666";
+
+            string result = "";
+
+            for (int i = 0; i < members.Length; ++i)
+            {
+                result += $"{members[i].id}, {members[i].Name}, {members[i].Phone}\n";
+            }
+
+            MessageBox.Show(result);
+        }
     }
 }
